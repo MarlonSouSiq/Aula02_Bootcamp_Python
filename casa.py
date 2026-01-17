09e1555249b99821ad5f383456458b79b1fa4b0f
Marlon Siqueira <marlonsousiq@gmail.com>
2026-01-17 01:12:43 -0300

    aula 2

---
+++ casa.py
@@ -0,0 +1,20 @@
+# 21: Conversor de Temperatura
+t_celsius=float(input("Entre com a temperaturs em Celsius: "))
+t_Fahrenheit= (t_celsius * 1.8) + 32
+print(f" A temperatua  {t_celsius} graus celsius equivale a {t_Fahrenheit} graus Fahrenheit")
+
+
+
+
+
+
+
+
+
+
+
+# 22: Verificador de Palíndromo
+# 23: Calculadora Simples
+# 24: Classificador de Números
+# 25: Conversão de Tipo com Validação
+
